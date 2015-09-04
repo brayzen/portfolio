b9575eb2cc40c7f5f66a09bde7169d55a49e674a
Brayzen <brian.ray.biz@gmail.com>
2015-09-04 00:14:49 -0700

    Added foundation, added index layout

---
+++ test/features/zurb_loaded_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+feature "Attached ZURB styles" do
+  scenario "Zurb css selectors will be verified" do
+    #Given: a home page
+    visit '/'
+    #When: loaded up or started
+    page.must_have_content "CEO's were thought"
+    #Then: should contain a link to zurb assets or columns
+    page.has_css? 'nav.top-bar'
+  end
+end
