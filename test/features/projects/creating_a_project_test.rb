8a27686e5494592b3a9a30fe66bd2bac4d1d8f23
Brayzen <brian.ray.biz@gmail.com>
2015-09-08 17:21:50 -0700

    renamed happy_path to 'creating_a_project_test.rb'

---
+++ test/features/projects/creating_a_project_test.rb
@@ -0,0 +1,16 @@
+
+require "test_helper"
+
+feature "As the site owner, I want to add a portfolio item so that I can show off my work" do
+  scenario "adding a new project" do
+    visit projects_path
+    click_on "New Project"
+    fill_in "Name", with: "Code Fellows Portfolio"
+    fill_in "Technologies used", with: "Rails, Ruby, Bootstrap, HTML5, CSS3"
+    click_on "Create Project"
+    page.text.must_include "Project has been created"
+    assert page.has_css?("#notice"), "Expected a flash notice on this page, none found."
+    page.status_code.must_equal 200
+  end
+end
+
