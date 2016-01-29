e330a97bd54376b05490ea38f65bf3e50b6c74f4
brayzen <brian.ray.biz@gmail.com>
2016-01-29 00:46:31 -0800

    add animation, speakerbox and scrolltop for rotate

---
+++ app/assets/javascripts/animations.js
@@ -0,0 +1,28 @@
+$(function(){
+  var answers = ['You like that mug shot?', 'Oh, you really do', 'Woa', 'Okay now its just Creepy', 'Creeper']
+  var $mypic = $('#mypic');
+  var count = 0;
+  //Scroll animiations
+  $(window).on('scroll', function(){
+    var $scrollTop = $(this).scrollTop();
+    if ($scrollTop > 230){
+      $mypic.addClass('rotate');
+    } else if ($scrollTop < 229) {
+      $mypic.removeClass('rotate');
+    }
+
+  })
+
+  //MOUSEOVER animations
+  $mypic.mouseover(function(){
+    var $speakerbox = $('#speakerbox');
+    $speakerbox.stop().css('display', 'block').css('opacity', '1');
+    count++;
+    console.log('this is the count: ' + count);
+    if (count >= 5) {
+      $speakerbox.text(answers[4]).fadeOut(3000).css('display', 'block');
+    } else {
+      $speakerbox.text(answers[count - 1]).fadeOut(3000).css('display', 'block');
+    }
+  })
+})
